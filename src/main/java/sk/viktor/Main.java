285a2ac9f6db1b6eb47aa17fa88914ddbc758119
unknown <Viktor_Meszaros@WN7X64-2609VM1.bra.emea.dell.com>
2012-11-15 18:01:38 +0100

    ++

--- src/main/java/sk/viktor/Main.java
+++ src/main/java/sk/viktor/Main.java
@@ -2,31 +2,16 @@ package sk.viktor;
 
 import java.lang.reflect.Method;
 
+import sk.viktor.server.SwingServer;
+
 
 public class Main {
     
     public static void main(String[] args) throws Exception {
-        SwingClassloader cl = new SwingClassloader();
-        Class<?> clazz = cl.loadClass("sk.viktor.server.SwingServer");
-        Method startServer = clazz.getMethod("startServer");
-        startServer.invoke(null);
+        SwingServer.startServer();
     }
     
-    public static void startSwing() throws Exception{
-        Class<?> clazz = Main.class.getClassLoader().loadClass("com.sun.swingset3.SwingSet3");
-        //Class<?> clazz = cl.loadClass("sk.viktor.Ceiling");
-        // Get a class representing the type of the main method's argument
-        Class mainArgType[] = { (new String[0]).getClass() };
-        String progArgs[] = new String[0];
-
-        // Find the standard main method in the class
-        Method main = clazz.getMethod("main", mainArgType);
-
-        // Create a list containing the arguments -- in this case,
-        // an array of strings
-        Object argsArray[] = { progArgs };
-
-        // Call the method
-        main.invoke(null, argsArray);
-    }
+    
+    
+  
 }
