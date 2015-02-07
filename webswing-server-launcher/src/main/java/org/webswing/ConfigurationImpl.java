dd2511a0528b05ee8fb1af423ceede95461140bf
Viktor Meszaros <viktor.meszaros2@gmail.com>
2015-02-07 14:29:10 +0000

    Tempdir; don't create new directories if CREATE_NEW_TEMP is false (merged from johannesreinhard)

--- webswing-server-launcher/src/main/java/org/webswing/ConfigurationImpl.java
+++ webswing-server-launcher/src/main/java/org/webswing/ConfigurationImpl.java
@@ -6,81 +6,84 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.webswing.util.Logger;
 
 public class ConfigurationImpl extends Configuration {
 
-    private String host = "localhost";
-    private String port = "8080";
-    private String configFile;
-    private String usersFile;
+	private String host = "localhost";
+	private String port = "8080";
+	private String configFile;
+	private String usersFile;
 
-    public String getHost() {
-        return host;
-    }
+	public String getHost() {
+		return host;
+	}
 
-    public String getPort() {
-        return port;
-    }
+	public String getPort() {
+		return port;
+	}
 
-    protected void setHost(String host) {
-        this.host = host;
-    }
+	protected void setHost(String host) {
+		this.host = host;
+	}
 
-    protected void setPort(String port) {
-        this.port = port;
-    }
+	protected void setPort(String port) {
+		this.port = port;
+	}
 
-    public String getConfigFile() {
-        return configFile;
-    }
+	public String getConfigFile() {
+		return configFile;
+	}
 
-    public void setConfigFile(String configFile) {
-        this.configFile = configFile;
-    }
+	public void setConfigFile(String configFile) {
+		this.configFile = configFile;
+	}
 
-    public String getUsersFile() {
-        return usersFile;
-    }
+	public String getUsersFile() {
+		return usersFile;
+	}
 
-    public void setUsersFile(String usersFile) {
-        this.usersFile = usersFile;
-    }
+	public void setUsersFile(String usersFile) {
+		this.usersFile = usersFile;
+	}
 
-    public static Configuration parse(String[] args) {
-        ConfigurationImpl cimpl = (ConfigurationImpl) Configuration.getInstance();
-        // create the command line parser
-        CommandLineParser parser = new PosixParser();
+	public static Configuration parse(String[] args) {
+		ConfigurationImpl cimpl = (ConfigurationImpl) Configuration.getInstance();
+		// create the command line parser
+		CommandLineParser parser = new PosixParser();
 
-        // create the Options
-        Options options = new Options();
-        options.addOption("h", "host", true, "Local interface address where the web server will listen. (localhost)");
-        options.addOption("p", "port", true, "Port where the web server will listen. (8080)");
-        options.addOption("t", "temp", true, "Temp folder for the webswing server. (./tmp)");
-        options.addOption("c", "config", true, "Configuration file name. (<webswing-server.war path>/webswing.config)");
-        options.addOption("u", "users", true, "Users properties file name. (<webswing-server.war path>/users.properties)");
+		// create the Options
+		Options options = new Options();
+		options.addOption("h", "host", true, "Local interface address where the web server will listen. (localhost)");
+		options.addOption("p", "port", true, "Port where the web server will listen. (8080)");
+		options.addOption("t", "temp", true, "The folder where temp folder will be created for the webswing server. (./tmp)");
+		options.addOption("d", true, "Create new temp folder for every webswing instance (default = false)");
+		options.addOption("c", "config", true, "Configuration file name. (<webswing-server.war path>/webswing.config)");
+		options.addOption("u", "users", true, "Users properties file name. (<webswing-server.war path>/users.properties)");
 
-        try {
-            // parse the command line arguments
-            CommandLine line = parser.parse(options, args);
+		try {
+			// parse the command line arguments
+			CommandLine line = parser.parse(options, args);
 
-            if (line.getOptionValue('h') != null) {
-                cimpl.setHost(line.getOptionValue('h'));
-            }
-            if (line.getOptionValue('p') != null) {
-                cimpl.setPort(line.getOptionValue('p'));
-            }
-            if (line.getOptionValue('c') != null) {
-                cimpl.setConfigFile(line.getOptionValue('c'));
-            }
-            if (line.getOptionValue('u') != null) {
-                cimpl.setUsersFile(line.getOptionValue('u'));
-            }
-        } catch (ParseException exp) {
-            System.out.println(exp.getMessage());
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("webswing", options);
-        }
-        return cimpl;
-    }
+			if (line.getOptionValue('h') != null) {
+				cimpl.setHost(line.getOptionValue('h'));
+			}
+			if (line.getOptionValue('p') != null) {
+				cimpl.setPort(line.getOptionValue('p'));
+			}
+			if (line.getOptionValue('c') != null) {
+				cimpl.setConfigFile(line.getOptionValue('c'));
+			}
+			if (line.getOptionValue('u') != null) {
+				cimpl.setUsersFile(line.getOptionValue('u'));
+			}
+			// NOTE: -d and -t are parsed in main.Main
+		} catch (ParseException exp) {
+			Logger.info(exp.getMessage());
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("webswing", options);
+		}
+		return cimpl;
+	}
 
 }
--- webswing-server/src/main/java/main/Main.java
+++ webswing-server/src/main/java/main/Main.java
@@ -27,152 +27,185 @@ import org.webswing.toolkit.WebToolkit;
 
 public class Main {
 
-    @SuppressWarnings("restriction")
-    public static void main(String[] args) throws Exception {
-        boolean client = System.getProperty(Constants.SWING_START_SYS_PROP_CLIENT_ID) != null;
+	@SuppressWarnings("restriction")
+	public static void main(String[] args) throws Exception {
+		boolean client = System.getProperty(Constants.SWING_START_SYS_PROP_CLIENT_ID) != null;
+		System.setProperty(Constants.CREATE_NEW_TEMP, getCreateNewTemp(args));
 
-        ProtectionDomain domain = Main.class.getProtectionDomain();
-        URL location = domain.getCodeSource().getLocation();
-        System.setProperty(Constants.WAR_FILE_LOCATION, location.toExternalForm());
+		ProtectionDomain domain = Main.class.getProtectionDomain();
+		URL location = domain.getCodeSource().getLocation();
+		System.setProperty(Constants.WAR_FILE_LOCATION, location.toExternalForm());
 
-        List<URL> urls = new ArrayList<URL>();
-        if (client) {
-            //initialize jmx agent
-            sun.management.Agent.startAgent();
+		List<URL> urls = new ArrayList<URL>();
+		if (client) {
+			// initialize jmx agent
+			sun.management.Agent.startAgent();
 
-            populateClasspathFromDir("WEB-INF/swing-lib", urls);
-            initializeExtLibServices(urls);
-            retainOnlyLauncherUrl(urls);
-        } else {
-            initTempDirPath(args);
-            populateClasspathFromDir("WEB-INF/server-lib", urls);
-        }
-        ClassLoader defaultCL = new URLClassLoader(urls.toArray(new URL[0]), null);
-        Thread.currentThread().setContextClassLoader(defaultCL);
-        Class<?> mainClass;
-        if (client) {
-            mainClass = defaultCL.loadClass("org.webswing.SwingMain");
-        } else {
-            mainClass = defaultCL.loadClass("org.webswing.ServerMain");
-        }
+			populateClasspathFromDir("WEB-INF/swing-lib", urls);
+			initializeExtLibServices(urls);
+			retainOnlyLauncherUrl(urls);
+		} else {
+			initTempDirPath(args);
+			populateClasspathFromDir("WEB-INF/server-lib", urls);
+		}
+		ClassLoader defaultCL = new URLClassLoader(urls.toArray(new URL[0]), null);
+		Thread.currentThread().setContextClassLoader(defaultCL);
+		Class<?> mainClass;
+		if (client) {
+			mainClass = defaultCL.loadClass("org.webswing.SwingMain");
+		} else {
+			mainClass = defaultCL.loadClass("org.webswing.ServerMain");
+		}
 
-        Method method = mainClass.getMethod("main", new Class[] { args.getClass() });
-        method.setAccessible(true);
-        try {
-            method.invoke(null, new Object[] { args });
-        } catch (IllegalAccessException e) {
-            // This should not happen, as we have
-            // disabled access checks
-        }
-    }
+		Method method = mainClass.getMethod("main", new Class[] { args.getClass() });
+		method.setAccessible(true);
+		try {
+			method.invoke(null, new Object[] { args });
+		} catch (IllegalAccessException e) {
+			// This should not happen, as we have
+			// disabled access checks
+		}
+	}
 
-    private static void retainOnlyLauncherUrl(List<URL> urls) {
-        for (Iterator<URL> i = urls.iterator(); i.hasNext();) {
-            URL thisurl = i.next();
-            if (!thisurl.getFile().contains("webswing-app-launcher")) {
-                i.remove();
-            }
-        }
+	public static String getCreateNewTemp(String[] args) {
+		// create the command line parser
+		for (int i = 0; i < args.length; i += 2) {
+			if (args[i].equals("-d") && i + 1 < args.length) {
+				return args[i + 1];
+			}
+		}
+		return "false";
 
-    }
+	}
 
-    private static void initializeExtLibServices(List<URL> urls) throws Exception {
-        ClassLoader extLibClassLoader = new URLClassLoader(urls.toArray(new URL[0]), null);
-        Class<?> classLoaderUtilclass = extLibClassLoader.loadClass("org.webswing.util.ClassLoaderUtil");
-        Method initializeServicesMethod = classLoaderUtilclass.getMethod("initializeServices");
-        initializeServicesMethod.invoke(null);
-        ((WebToolkit) Toolkit.getDefaultToolkit()).init();
-    }
+	private static void retainOnlyLauncherUrl(List<URL> urls) {
+		for (Iterator<URL> i = urls.iterator(); i.hasNext();) {
+			URL thisurl = i.next();
+			if (!thisurl.getFile().contains("webswing-app-launcher")) {
+				i.remove();
+			}
+		}
 
-    private static void populateClasspathFromDir(String dir, List<URL> urls) throws IOException, MalformedURLException {
-        for (URL f : getFilesFromPath(Main.class.getClassLoader().getResource(dir))) {
-            urls.add(f);
-        }
-    }
+	}
 
-    private static List<URL> getFilesFromPath(URL r) throws IOException {
-        List<URL> urls = new ArrayList<URL>();
-        String[] splitPath = r.getPath().split("\\!/");
-        String jar = splitPath[0];
-        String path = splitPath[1];
-        JarFile jarFile = new JarFile(new File(URI.create(jar)));
-        Enumeration<JarEntry> jarEntries = jarFile.entries();
-        while (jarEntries.hasMoreElements()) {
-            JarEntry jarEntry = jarEntries.nextElement();
-            if (!jarEntry.isDirectory() && jarEntry.getName().endsWith(".jar") && jarEntry.getName().startsWith(path)) {
-                urls.add(jarEntryAsFile(jarFile, jarEntry).toURI().toURL());
-            }
-        }
-        return urls;
-    }
+	private static void initializeExtLibServices(List<URL> urls) throws Exception {
+		ClassLoader extLibClassLoader = new URLClassLoader(urls.toArray(new URL[0]), null);
+		Class<?> classLoaderUtilclass = extLibClassLoader.loadClass("org.webswing.util.ClassLoaderUtil");
+		Method initializeServicesMethod = classLoaderUtilclass.getMethod("initializeServices");
+		initializeServicesMethod.invoke(null);
+		((WebToolkit) Toolkit.getDefaultToolkit()).init();
+	}
 
-    private static File jarEntryAsFile(JarFile jarFile, JarEntry jarEntry) throws IOException {
-        InputStream input = null;
-        OutputStream output = null;
-        try {
-            String name = jarEntry.getName().replace('/', '_');
-            int i = name.lastIndexOf(".");
-            String extension = i > -1 ? name.substring(i) : "";
-            File file = new File(getTempDir().getAbsolutePath() + File.separator + name.substring(0, name.length() - extension.length()) + extension);
-            if (!file.exists()) {
-                file.createNewFile();
-                file.deleteOnExit();
-                input = jarFile.getInputStream(jarEntry);
-                output = new FileOutputStream(file);
-                int readCount;
-                byte[] buffer = new byte[4096];
-                while ((readCount = input.read(buffer)) != -1) {
-                    output.write(buffer, 0, readCount);
-                }
-            }
-            return file;
-        } finally {
-            close(input);
-            close(output);
-        }
-    }
+	private static void populateClasspathFromDir(String dir, List<URL> urls) throws IOException, MalformedURLException {
+		for (URL f : getFilesFromPath(Main.class.getClassLoader().getResource(dir))) {
+			urls.add(f);
+		}
+	}
 
-    private static void close(Closeable closeable) {
-        if (closeable != null) {
-            try {
-                closeable.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	private static List<URL> getFilesFromPath(URL r) throws IOException {
+		List<URL> urls = new ArrayList<URL>();
+		String[] splitPath = r.getPath().split("\\!/");
+		String jar = splitPath[0];
+		String path = splitPath[1];
+		JarFile jarFile = new JarFile(new File(URI.create(jar)));
+		Enumeration<JarEntry> jarEntries = jarFile.entries();
+		String tempDirPath = getTempDir().getAbsolutePath();
+		while (jarEntries.hasMoreElements()) {
+			JarEntry jarEntry = jarEntries.nextElement();
+			if (!jarEntry.isDirectory() && jarEntry.getName().endsWith(".jar") && jarEntry.getName().startsWith(path)) {
+				urls.add(jarEntryAsFile(jarFile, jarEntry, tempDirPath).toURI().toURL());
+			}
+		}
+		return urls;
+	}
 
-    public static File getTempDir() {
-        if (System.getProperty(Constants.TEMP_DIR_PATH) == null) {
-            File baseDir = new File(System.getProperty(Constants.TEMP_DIR_PATH_BASE, System.getProperty("java.io.tmpdir")));
-            if (!baseDir.exists()) {
-                baseDir.mkdirs();
-            }
-            SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd-HH-mm-ss");
-            String baseName = sdf.format(new Date()) + "-";
+	private static File jarEntryAsFile(JarFile jarFile, JarEntry jarEntry, String tempDirPath) throws IOException {
+		InputStream input = null;
+		OutputStream output = null;
+		try {
+			String name = jarEntry.getName();
+			if (name.contains("/")) {
+				name = name.replace('/', '_');
+				int i = name.lastIndexOf(".");
+				String extension = i > -1 ? name.substring(i) : "";
+				name = name.substring(0, name.length() - extension.length()) + extension;
+			}
+			File file = new File(tempDirPath + File.separator + name);
+			if (!file.exists()) {
+				file.createNewFile();
+				file.deleteOnExit();
+				input = jarFile.getInputStream(jarEntry);
+				output = new FileOutputStream(file);
+				int readCount;
+				byte[] buffer = new byte[4096];
+				while ((readCount = input.read(buffer)) != -1) {
+					output.write(buffer, 0, readCount);
+				}
+			}
+			return file;
+		} finally {
+			close(input);
+			close(output);
+		}
+	}
 
-            for (int counter = 0; counter < 10; counter++) {
-                File tempDir = new File(baseDir, baseName + counter);
-                if (tempDir.mkdir()) {
-                    System.setProperty(Constants.TEMP_DIR_PATH, tempDir.toURI().toString());
-                    return tempDir;
-                }
-            }
-            throw new IllegalStateException("Failed to create directory within " + 10 + " attempts (tried " + baseName + " to " + baseName + (100 - 1) + ')');
-        } else {
-            return new File(URI.create(System.getProperty(Constants.TEMP_DIR_PATH)));
-        }
-    }
+	private static void close(Closeable closeable) {
+		if (closeable != null) {
+			try {
+				closeable.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    private static void initTempDirPath(String[] args) {
-        if (args != null) {
-            for (int i = 0; i < args.length - 1; i++) {
-                if ("-t".equals(args[i]) || "-temp".equals(args[i])) {
-                    System.setProperty(Constants.TEMP_DIR_PATH_BASE, args[i + 1]);
-                    return;
-                }
-            }
-        }
-        System.setProperty(Constants.TEMP_DIR_PATH_BASE, "tmp");
-    }
+	public static File getTempDir() {
+		if (System.getProperty(Constants.TEMP_DIR_PATH) == null) {
+			File baseDir = new File(System.getProperty(Constants.TEMP_DIR_PATH_BASE, System.getProperty("java.io.tmpdir")));
+			if (!baseDir.exists()) {
+				baseDir.mkdirs();
+			}
+			SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd-HH-mm-ss");
+			String baseName;
+			if (Boolean.parseBoolean(System.getProperty(Constants.CREATE_NEW_TEMP, ""))) {
+				baseName = sdf.format(new Date()) + "-";
+				for (int counter = 0; counter < 10; counter++) {
+					File tempDir = new File(baseDir, baseName + counter);
+					if (tempDir.mkdir()) {
+						System.setProperty(Constants.TEMP_DIR_PATH, tempDir.toURI().toString());
+						return tempDir;
+					}
+				}
+			} else {
+				baseName = "release";
+				File tempDir = new File(baseDir, baseName);
+				if (!tempDir.exists()) {
+					tempDir.mkdir();
+				} else {
+					for (File f : tempDir.listFiles()) {
+						if (!f.delete()) {
+							throw new IllegalStateException("Not possible to clean the temp folder. Make sure no other instance of webswing is running or use '-d true' option to create a new temp folder.");
+						}
+					}
+				}
+				System.setProperty(Constants.TEMP_DIR_PATH, tempDir.toURI().toString());
+				return tempDir;
+			}
+			throw new IllegalStateException("Failed to create directory within " + 10 + " attempts (tried " + baseName + " to " + baseName + (100 - 1) + ')');
+		} else {
+			return new File(URI.create(System.getProperty(Constants.TEMP_DIR_PATH)));
+		}
+	}
+
+	private static void initTempDirPath(String[] args) {
+		if (args != null) {
+			for (int i = 0; i < args.length - 1; i++) {
+				if ("-t".equals(args[i]) || "-temp".equals(args[i])) {
+					System.setProperty(Constants.TEMP_DIR_PATH_BASE, args[i + 1]);
+					return;
+				}
+			}
+		}
+		System.setProperty(Constants.TEMP_DIR_PATH_BASE, "tmp");
+	}
 }
