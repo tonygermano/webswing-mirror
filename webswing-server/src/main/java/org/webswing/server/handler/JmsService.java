46dd80c01e3b046f1635246c51cc0fa19d1833d6
Viktor Meszaros <viktor.meszaros2@gmail.com>
2015-02-12 22:34:31 +0000

    memory consumption optimization

--- webswing-server/src/main/java/org/webswing/server/handler/JmsService.java
+++ webswing-server/src/main/java/org/webswing/server/handler/JmsService.java
@@ -11,55 +11,55 @@ import org.apache.activemq.usage.MemoryUsage;
 import org.apache.activemq.usage.SystemUsage;
 import org.webswing.Constants;
 
-
 public class JmsService implements ServletContextListener {
 
-    private BrokerService broker;
+	private BrokerService broker;
 
-    public void contextInitialized(ServletContextEvent event) {
-        try {
-            broker = startService();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public void contextInitialized(ServletContextEvent event) {
+		try {
+			broker = startService();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void contextDestroyed(ServletContextEvent event) {
-        try {
-            broker.stop();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public BrokerService startService() throws Exception {
-        // BrokerService broker= BrokerFactory.createBroker("xbean:mq.xml");
+	public void contextDestroyed(ServletContextEvent event) {
+		try {
+			broker.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        BrokerService broker = new BrokerService();
-        broker.setUseJmx(true);
-        broker.setPersistent(false);
+	public BrokerService startService() throws Exception {
+		// BrokerService broker= BrokerFactory.createBroker("xbean:mq.xml");
 
-        PolicyMap policyMap = new PolicyMap();
-        PolicyEntry defaultEntry = new PolicyEntry();
-        ConstantPendingMessageLimitStrategy pendingMessageLimitStrategy = new ConstantPendingMessageLimitStrategy();
-        pendingMessageLimitStrategy.setLimit(10);
-        defaultEntry.setPendingMessageLimitStrategy(pendingMessageLimitStrategy);
-        policyMap.setDefaultEntry(defaultEntry);
-        broker.setDestinationPolicy(policyMap);
+		BrokerService broker = new BrokerService();
+		broker.setUseJmx(true);
+		broker.setPersistent(false);
 
-        SystemUsage memoryManager = new SystemUsage();
-        MemoryUsage memoryLimit = new MemoryUsage();
-        memoryLimit.setLimit(80000000);
+		PolicyMap policyMap = new PolicyMap();
+		PolicyEntry defaultEntry = new PolicyEntry();
 
-        memoryManager.setMemoryUsage(memoryLimit);
-        broker.setSystemUsage(memoryManager);
-        // configure the broker
-        broker.addConnector(Constants.JMS_URL);
+		ConstantPendingMessageLimitStrategy pendingMessageLimitStrategy = new ConstantPendingMessageLimitStrategy();
+		pendingMessageLimitStrategy.setLimit(10);
+		defaultEntry.setPendingMessageLimitStrategy(pendingMessageLimitStrategy);
+		defaultEntry.setMemoryLimit(5 * 1024 * 1024);
 
-        broker.start();
-        return broker;
-                
-    }
+		policyMap.setDefaultEntry(defaultEntry);
+		broker.setDestinationPolicy(policyMap);
+
+		SystemUsage memoryManager = new SystemUsage();
+		MemoryUsage memoryLimit = new MemoryUsage();
+		memoryLimit.setLimit(80 * 1024 * 1024);
+
+		memoryManager.setMemoryUsage(memoryLimit);
+		broker.setSystemUsage(memoryManager);
+		// configure the broker
+		broker.addConnector(Constants.JMS_URL);
+
+		broker.start();
+		return broker;
+
+	}
 }
-
-    
