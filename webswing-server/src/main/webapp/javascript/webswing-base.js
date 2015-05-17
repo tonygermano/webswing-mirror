31fd281f25c7ea42caa22100bbd7307cc935e621
Viktor Meszaros <viktor.meszaros2@gmail.com>
2015-05-17 19:10:10 +0100

    resolves Issue #65 : paste (ctrl+v) operation adds v chars 3 -ie hack

--- webswing-server/src/main/webapp/javascript/webswing-base.js
+++ webswing-server/src/main/webapp/javascript/webswing-base.js
@@ -19,6 +19,7 @@
 
 	var windowImageHolders = {};
 	var directDraw = new WebswingDirectDraw({});
+	var iePastePromptHack=false;
 
 	function startApplication(name) {
 		api.canvas.get();
@@ -370,6 +371,7 @@
 			// hanle paste event
 			if (keyevt.key.ctrl && keyevt.key.character == 86) { // ctrl+v
 				var text = prompt('Press ctrl+v and enter..');
+				iePastePromptHack=true;setTimeout(function(){iePastePromptHack=false;},10);
 				if (api.context.hasControl) {
 					api.socket.send({
 						paste : {
@@ -391,7 +393,7 @@
 			event.preventDefault();
 			event.stopPropagation();
 			var keyevt = getKBKey('keypress', canvas, event);
-			if (!(keyevt.key.ctrl &&  keyevt.key.character == 118)) { // skip ctrl+v
+			if (!(keyevt.key.ctrl &&  keyevt.key.character == 118) && !(iePastePromptHack &&  keyevt.key.character == 118)) { // skip ctrl+v
 				enqueueInputEvent(keyevt);
 			}
 			return false;
