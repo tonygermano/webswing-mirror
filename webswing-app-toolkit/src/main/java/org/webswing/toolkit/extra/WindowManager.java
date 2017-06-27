fa09291fc26adad5d27cc1b84bb8dda78333f09c
Viktor Meszaros <viktor.meszaros2@gmail.com>
2017-06-27 22:52:27 +0200

    resolves #235 :focus back to most recent after window closed

--- webswing-app-toolkit/src/main/java/org/webswing/toolkit/extra/WindowManager.java
+++ webswing-app-toolkit/src/main/java/org/webswing/toolkit/extra/WindowManager.java
@@ -170,7 +170,7 @@ public class WindowManager {
 	}
 
 	public void activateWindow(Window w, int x, int y) {
-		Component newFocusOwner = w.getFocusTraversalPolicy().getInitialComponent(w);
+		Component newFocusOwner = w.getMostRecentFocusOwner();
 		activateWindow(w, newFocusOwner, x, y, false, true, CausedFocusEvent.Cause.NATIVE_SYSTEM);
 	}
 
