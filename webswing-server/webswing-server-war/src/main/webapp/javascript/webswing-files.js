d8ab43db910be66569656e19b0958e9fb7d5a774
Viktor.meszaros <viktor.meszaros@webswing.org>
2018-01-15 16:09:52 +0000

    simultaneous multiple file download

--- webswing-server/webswing-server-war/src/main/webapp/javascript/webswing-files.js
+++ webswing-server/webswing-server-war/src/main/webapp/javascript/webswing-files.js
@@ -351,14 +351,15 @@ define(['jquery', 'text!templates/upload.html', 'jquery.iframe-transport', 'jque
         }
 
         function download(url) {
-            var hiddenIFrameID = 'hiddenDownloader', iframe = document.getElementById(hiddenIFrameID);
-            if (iframe === null) {
-                iframe = document.createElement('iframe');
-                iframe.id = hiddenIFrameID;
-                iframe.style.display = 'none';
-                document.body.appendChild(iframe);
-            }
-            iframe.src = api.cfg.connectionUrl + url;
+             var hiddenIFrameID = 'hiddenDownloader'+url, iframe = document.getElementById(hiddenIFrameID);
+             if(iframe!=null){
+                iframe.parentNode.removeChild(iframe);
+             }
+             iframe = document.createElement('iframe');
+             iframe.id = hiddenIFrameID;
+             iframe.style.display = 'none';
+             document.body.appendChild(iframe);
+             iframe.src = api.cfg.connectionUrl + url;
         }
 
         function link(url) {
