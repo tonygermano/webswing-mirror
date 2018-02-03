86a564c61d6f4d96205e9313fd8fac307035fb0f
Andrej Zelman <andrej.zelman@gmail.com>
2018-02-03 16:09:52 +0100

    Fixes Serialization Exception for Property File security mode

---
+++ webswing-server/webswing-server-security/src/main/java/org/webswing/server/services/security/modules/property/PropertiesRealmSerializable.java
@@ -0,0 +1,11 @@
+package org.webswing.server.services.security.modules.property;
+
+import java.io.Serializable;
+
+import org.apache.shiro.realm.text.PropertiesRealm;
+
+public class PropertiesRealmSerializable extends PropertiesRealm implements Serializable {
+
+	private static final long serialVersionUID = -5279888126504918390L;
+
+}
