8928d424644ce66f05872173e10059892f940423
Viktor Meszaros <viktor.meszaros@webswing.org>
2019-01-22 03:31:26 +0100

    fix authorization for metrics endpoint

--- webswing-server/webswing-server-war/src/main/java/org/webswing/server/services/rest/resources/SwingAppRestService.java
+++ webswing-server/webswing-server-war/src/main/java/org/webswing/server/services/rest/resources/SwingAppRestService.java
@@ -126,7 +126,8 @@ public class SwingAppRestService extends BaseRestService {
 	@GET
     @Path("/rest/metrics/{uuid}")
     public SwingSession getMetrics(@PathParam("uuid") String uuid) throws WsException {
-        SwingInstance instance = manager.getSwingInstanceHolder().findInstanceByConnectionId(uuid);
+		getHandler().checkPermissionLocalOrMaster(WebswingAction.websocket_connect);
+		SwingInstance instance = manager.getSwingInstanceHolder().findInstanceByConnectionId(uuid);
         if (instance != null && getHandler().getUser().getUserId().equals(instance.getUserId())) {
             return instance.toSwingSession(true);
         }
