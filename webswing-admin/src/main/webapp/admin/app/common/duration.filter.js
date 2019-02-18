b2de34de8c262a34c71db476a0316d536a53696b
Viktor Meszaros <viktor.meszaros@webswing.org>
2019-02-18 13:30:31 +0100

    fix duration displaying

--- webswing-admin/src/main/webapp/admin/app/common/duration.filter.js
+++ webswing-admin/src/main/webapp/admin/app/common/duration.filter.js
@@ -1,11 +1,12 @@
 (function (define) {
     define([], function f() {
         function durationFilter() {
-            return function (number) {
+            return function(number) {
                 var secondsTotal = number / 1000;
                 var seconds = secondsTotal % 60;
                 var minutes = ((secondsTotal - seconds) / 60) % 60;
-                return minutes + ' min ' + Math.round(seconds) + ' sec';
+                var hours = (secondsTotal - minutes * 60 - seconds) / 3600;
+                return (hours > 0?(hours +'h '):'')+minutes + 'min ' + Math.round(seconds) + 'sec';
             };
         }
         return durationFilter;
